ef917ebdfb1e2bbab7ea9d08d2efde4160d06a07
Carlos <ramos0810eduardo@gmail.com>
2021-11-27 13:23:51 -0300

    inserção campo descrição quarto

--- src/controllers/QuartosController.js
+++ src/controllers/QuartosController.js
@@ -20,14 +20,14 @@ module.exports = {
 
         const [nome_quarto, valor_quarto, quantia_camas, quantia_banheiros, img_quarto, descricao_quarto] = [Nome_quarto, Valor_quarto, Quantia_camas, Quantia_banheiros, Img_quarto, Descricao_quarto];
 
-        // console.log("Merda 2:", req.body);
+        //console.log("Merda 2:", req.body);
 
         try{
 
             // const quarto_print = {nome_quarto, valor_quarto, quantia_camas, quantia_banheiros, img_quarto};
-            // console.log("Aqui está essa merda:", quarto_print);
+            //console.log("Aqui está essa merda:", quarto_print);
 
-            const quarto = await Quartos.create({nome_quarto, valor_quarto, quantia_camas, quantia_banheiros, img_quarto});
+            const quarto = await Quartos.create({nome_quarto, valor_quarto, quantia_camas, quantia_banheiros, img_quarto, descricao_quarto});
             return res.json(quarto);
         }catch(err){
             console.log(err);
--- src/database/migrations/20211029011815-create-quartos.js
+++ src/database/migrations/20211029011815-create-quartos.js
@@ -29,6 +29,10 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false
       },
+      descricao_quarto: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
       created_at: {
         type: Sequelize.DATE,
         allowNull: false
