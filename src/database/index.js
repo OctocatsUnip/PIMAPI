c7594bd35eedacb97f47c2532c39d6f410f6ece0
Thayane Bomfim <thayane_bomfims@hotmail.com>
2021-11-30 00:34:51 -0300

    Exclusão migrations, controllers e Models

--- src/database/index.js
+++ src/database/index.js
@@ -5,23 +5,7 @@ const dbConfig = require('../config/database');
 const Pessoa = require('../models/Pessoa');
 const Endereco = require('../models/Endereco');
 const Quartos = require('../models/Quartos');
-const Cardapios = require('../models/Cardapios');
-const Pratos = require('../models/Pratos');
-const CardapioPratos = require('../models/CardapioPratos');
-const ListaPedidosComida = require('../models/ListaPedidosComida');
-const ListaPedidosServicos = require('../models/ListaPedidosServicos');
-const Cargos = require('../models/Cargos');
-const Telefones = require('../models/Telefones');
-const Beneficios = require('../models/Beneficios');
-const QuartoBeneficios = require('../models/QuartoBeneficios');
-const UsuarioSistema = require('../models/UsuarioSistema');
-const UsuarioCliente = require('../models/UsuarioCliente');
-const Pagantes = require('../models/Pagantes');
-const Fichas = require('../models/Fichas');
-const Checkouts = require('../models/Checkouts');
-const RequisicaoPedidos = require('../models/RequisicaoPedidos');
-const Hospedes = require('../models/Hospedes');
-const Funcionarios = require('../models/Funcionarios');
+
 
 //realizando a conexão com o banco
 const connection = new Sequelize(dbConfig);
@@ -30,37 +14,10 @@ const connection = new Sequelize(dbConfig);
 Pessoa.init(connection);
 Endereco.init(connection);
 Quartos.init(connection);
-Cardapios.init(connection);
-Pratos.init(connection);
-CardapioPratos.init(connection);
-ListaPedidosComida.init(connection);
-ListaPedidosServicos.init(connection);
-Cargos.init(connection);
-Telefones.init(connection);
-Beneficios.init(connection);
-QuartoBeneficios.init(connection);
-UsuarioSistema.init(connection);
-UsuarioCliente.init(connection);
-Pagantes.init(connection);
-Fichas.init(connection);
-Checkouts.init(connection);
-RequisicaoPedidos.init(connection);
-Hospedes.init(connection);
-Funcionarios.init(connection);
 
-//aqui inicializamos os metodos
-Pessoa.associate(connection.models);
-Endereco.associate(connection.models); //aqui podemos passar cada model separadamente ou passar assim para chamar tudo de uma vez
-CardapioPratos.associate(connection.models);
-Telefones.associate(connection.models);
-UsuarioSistema.associate(connection.models);
-Cargos.associate(connection.models);
-UsuarioCliente.associate(connection.models);
-Fichas.associate(connection.models);
-Checkouts.associate(connection.models);
-RequisicaoPedidos.associate(connection.models);
-Hospedes.associate(connection.models);
-Funcionarios.associate(connection.models);
+
+//aqui inicializamos os metodos de associação
+
 
 //avaliar utilizar o consign ou o require-directory para importar automaticamente e inicializar as models
 
--- src/routes.js
+++ src/routes.js
@@ -3,15 +3,7 @@ const express = require('express');
 
 const PessoaController = require('./controllers/PessoaController');
 const EnderecoController = require('./controllers/EnderecoController');
-const TelefonesController = require('./controllers/TelefonesController');
 const QuartosController = require('./controllers/QuartosController');
-const CardapiosController = require('./controllers/CardapiosController');
-const PratosController = require('./controllers/PratosController');
-const ListaPedidosComidaController = require('./controllers/ListaPedidosComidaController');
-const ListaPedidosServicosController = require('./controllers/ListaPedidosServicosController');
-const CargosController = require('./controllers/CargosController');
-const BeneficiosController = require('./controllers/BeneficiosController');
-const UsuarioSistemaController = require('./controllers/UsuarioSistemaController');
 
 const routes = express.Router();
 
@@ -22,32 +14,9 @@ routes.get('/pessoa/:cpf', PessoaController.findByCpf);
 routes.get('/pessoa/endereco/:pessoa_id', EnderecoController.index);
 routes.post('/pessoa/endereco/:pessoa_id', EnderecoController.store);
 
-routes.get('/pessoa/telefones/:pessoa_id', TelefonesController.index);
-routes.post('/pessoa/telefones/:pessoa_id', TelefonesController.store);
-
 routes.get('/quarto', QuartosController.index);
 routes.post('/quarto', QuartosController.store);
 routes.post('/quarto/:quantia_banheiros', QuartosController.findByBanheiro);
 
-routes.get('/cardapio', CardapiosController.index);
-routes.post('/cardapio', CardapiosController.store);
-
-routes.get('/prato', PratosController.index);
-routes.post('/prato', PratosController.store);
-
-routes.get('/lista_pedidos_comida', ListaPedidosComidaController.index);
-routes.post('/lista_pedidos_comida', ListaPedidosComidaController.store);
-
-routes.get('/lista_pedidos_servicos', ListaPedidosServicosController.index);
-routes.post('/lista_pedidos_servicos', ListaPedidosServicosController.store);
-
-routes.get('/cargos', CargosController.index);
-routes.post('/cargos', CargosController.store);
-
-routes.get('/beneficios', BeneficiosController.index);
-routes.post('/beneficios', BeneficiosController.store);
-
-routes.get('/cargos/:cargo_id/usuario_sistema', UsuarioSistemaController.index);
-routes.post('/cargos/:cargo_id/usuario_sistema', UsuarioSistemaController.store);
 
 module.exports = routes;
